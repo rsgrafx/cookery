e2334a82c6c8a0a380eae080249ba81ff76f0c07
Orion Engleton <rsgrafx@gmail.com>
2018-03-27 16:07:13 -0700

    chore: Call correct functions after refactor

--- lib/cookery_web/resolvers/recipe.ex
+++ lib/cookery_web/resolvers/recipe.ex
@@ -2,18 +2,18 @@ defmodule CookeryWeb.Resolvers.Recipe do
   @moduledoc """
   Resolve Recipe Search queries
   """
-  alias Cookery.Recipe
+  alias Cookery.Recipes.Search
 
   def fetch_recipe(_, %{recipe_id: recipe_id}, _) do
-    {:ok, Cookery.Recipes.Search.fetch_by_id(recipe_id)}
+    {:ok, Search.fetch_by_id(recipe_id)}
   end
 
   def filter_recipes(_, %{matching: name}, _) do
-    {:ok, Cookery.Recipes.Search.find_with_term(name)}
+    {:ok, Search.find_with_term(name)}
   end
 
   def filter_recipes(_, _, _) do
-    {:ok, Recipe.all()}
+    {:ok, Search.stubbed_default()}
   end
 
 end
