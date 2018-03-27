8f29d3b3e5c85921aaa631e0210b2906fca70a91
Orion Engleton <rsgrafx@gmail.com>
2018-03-27 16:05:58 -0700

    refactor: Created Ingredient module
    Added functionality for building Ingredients, Added Ingredients module
    Moved functionality specific to Edamams api to Edamam module

--- lib/cookery/recipes/search.ex
+++ lib/cookery/recipes/search.ex
@@ -4,6 +4,7 @@ defmodule Cookery.Recipes.Search do
   """
 
   alias Cookery.Recipes
+  alias Cookery.Edamam
 
   def find_with_term(food_item) do
     %{body: body} = search_recipes(food_item)
@@ -12,25 +13,39 @@ defmodule Cookery.Recipes.Search do
     |> return_recipes()
   end
 
+  def fetch_by_id(recipe_id) do
+    recipe_id
+    |> Edamam.fetch_by_id_endpoint()
+    |> HTTPoison.get!()
+    |> case do
+      %{status_code: 200, body: body} ->
+        body
+        |> Poison.decode()
+        |> return_recipes()
+
+      _ -> return_recipes(:null)
+    end
+  end
+
   def return_recipes({:ok, %{"hits" => recipes}}) do
     Recipes.transform_to_recipes(recipes)
   end
 
-  def return_recipes(_), do: {:error, "Could not parse internal data"}
+  def return_recipes({:ok, [%{"uri" => _recipes_url} = recipe]}) do
+    Recipes.build_recipe(%{"recipe" => recipe})
+  end
+
+  def return_recipes(_),
+    do: %{error: "Could not parse internal data", status_code: 422}
 
   def search_recipes(food_item) do
     food_item
-    |> build_url()
+    |> Edamam.search_url()
     |> HTTPoison.get!()
   end
 
-  defp build_url(food_item) do
-    [
-      api_app_id: app_id,
-      api_secret_key: app_secret_key,
-      api_url: base_url
-    ] = Application.get_env(:cookery, :edamam)
-
-    "#{base_url}/search?q=#{food_item}&app_id=#{app_id}&app_key=#{app_secret_key}"
+  def stubbed_default() do
+    find_with_term("peanuts")
   end
+
 end
